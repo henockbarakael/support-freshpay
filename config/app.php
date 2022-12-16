fead02848306bf3647397bcbfa1e356f112f696f
unknown <barahenock@gmail.com>
2022-12-16 06:32:23 +0100

    Dashboard Support FreshPay 3

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://seashell-app-v5oo5.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL', null),
 

21cc16a594446d1fdf883e7b78df42e0dacb43ff
Henock BARAKAEL <barahenock@gmail.com>
2023-01-26 14:15:31 +0100

    Session

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'file'),
+    'driver' => env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'dashboard.gofreshpay.com'),
 
     /*
     |--------------------------------------------------------------------------
